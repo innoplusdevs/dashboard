c701ba569a346c504fb8b09ba2e2e3760003adeb
innoplusdevs <innoplusdevs@gmail.com>
2021-10-28 23:10:36 -0400

    DB config

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,1 +1,27 @@
 const mongoose = require('mongoose');
+
+
+
+const dbConnection = async () => {
+
+    try {
+        await mongoose.connect('mongodb://localhost/dashboard', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+
+        });
+
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error initializing database");
+    }
+
+
+
+}
+
+
+
+module.exports = dbConnection;
